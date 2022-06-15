f1e4f5d0bd397aa7d9f418a53cb417c080ccf3ba
bob bae <bob.bae@gmail.com>
2022-06-15 14:10:39 -0700

    cgo

---
+++ Go/cgo/greeter.h
@@ -0,0 +1,6 @@
+#ifndef _GREETER_H
+#define _GREETER_H
+
+int greet(const char *name, int year, char *out);
+
+#endif
---
+++ Go/cgo/main.go
@@ -0,0 +1,24 @@
+package main
+
+// #cgo CFLAGS: 
+// #include <stdlib.h>
+// #include "greeter.h"
+import "C"
+import (
+	"fmt"
+	"unsafe"
+)
+
+func main() {
+	name := C.CString("Gopher")
+	defer C.free(unsafe.Pointer(name))
+
+	year := C.int(2018)
+	ptr := C.malloc(C.sizeof_char * 1024)
+	defer C.free(unsafe.Pointer(ptr))
+	size := C.greet(name, year, (*C.char)(ptr))
+	b := C.GoBytes(ptr, size)
+	fmt.Println(string(b))
+}
+
+
